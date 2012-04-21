6a9f03d8af7095ec927de7467200618caa0a40ea
benjaoming <benjaoming@gmail.com>
2012-04-21 15:46:49 +0200

    Fix date formatting for data add form

--- arduinodataserver/forms.py
+++ arduinodataserver/forms.py
@@ -4,7 +4,8 @@ import models
 
 class MeterDataForm(forms.ModelForm):
     
-    created_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'datepicker'}),
+    created_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'datepicker',
+                                                                 'data-date-format': 'yyyy-mm-dd'}),
                                    input_formats=["%Y-%m-%d"])
     created_time = forms.TimeField(initial="00:00")
     
